d5b080ef7fe3060801f58cd548be47cf23b66052
Fabien Bounoir <71253960+FabienBounoir@users.noreply.github.com>
2023-03-28 11:43:18 +0200

    🐛 Fix bug var

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ const client = new tmi.Client({
 client.on('message', (channel, tags, message, self) => {
     if (self) return; // ignore les messages envoyés par le bot lui-même
 
-    let channel = channel.replace("#", "")
+    console.log(`${tags.username}: ${message}`)
+
+    channel = channel.replace("#", "")
     if (filteredBot.includes(tags.username) || (tags.username == channel)) return; // ignore les messages envoyés par les bots filtrés
     fs.appendFileSync(`./tchat/${channel}.txt`, `${message}\n`); // enregistrement du message dans un fichier
     fs.appendFileSync(`./user/${channel}.txt`, `${tags.username}\n`); // enregistrement du nom d'utilisateur dans un fichier
